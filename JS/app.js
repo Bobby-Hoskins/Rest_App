3cf3f25c20d5467b7af1b36e4481464471403900
Bobby-Hoskins <bobbyhoskins1986@gmail.com>
2021-10-23 12:04:53 -0400

    Added Functionality to Mobile Hamburger Menu

---
+++ JS/app.js
@@ -0,0 +1,21 @@
+const mobileHamburger = document.querySelector(".mobile-hamburger");
+const navMenu = document.querySelector(".nav-menu");
+
+mobileHamburger.addEventListener("click", mobileMenu);
+
+function mobileMenu() {
+    mobileHamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+}
+
+
+const navLink = document.querySelectorAll(".nav-link");
+
+navLink.forEach(n => n.addEventListener("click", closeMenu));
+
+function closeMenu() {
+    mobileHamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+}
+
+
