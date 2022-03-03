66a2f30023cab886186b90ab6ef7849cedb68376
Bobby-Hoskins <bobbyhoskins1986@gmail.com>
2022-03-03 15:48:40 -0500

    Added Git LFS

--- JS/app.js
+++ JS/app.js
@@ -102,4 +102,32 @@ song.ontimeupdate = function () {
         video.pause();
     }
 };
-app();
+
+// Contact Page
+const form = document.getElementById('form');
+const firstName = document.getElementById('first-name');
+const lastName = document.getElementById('last-name');
+const email = document.getElementById('email');
+const userMessage = document.getElementById('message');
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+});
+
+function checkInputs() {
+    const firstNameValue = firstName.value.trim();
+    const lastNameValue = lastName.value.trim();
+    const emailValue = email.value.trim();
+    const userMessageValue = userMessage.value.trim();
+
+    if (firstNameValue === '') {
+        setErrorFor(firstName, 'First Name Cannot be Blank')
+    } else {
+        setSuccessFor(firstNameValue)
+    }
+}
+
+function setErrorFor(input, message) {
+    const formControl = input.parentElement;
+}
